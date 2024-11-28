e7148feff84750967a80a8f19210703e66ae20d1
Cyndie <oguanyiacynthia@gmail.com>
2024-11-28 09:56:50 +0100

    Second

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -189,12 +189,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
